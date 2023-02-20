2a4a977ae3059c27737c703c482eea248b87fb7e
John Doe <cformation75@gmail.com>
2023-02-21 00:22:51 +0100

    cccc

--- src/pages/index.js
+++ src/pages/index.js
@@ -127,7 +127,7 @@ const IndexPage = () => {
   return (
     <main style={pageStyles}>
       <h1 style={headingStyles}>
-        Congratulation family news
+        Congratulation family news without github
         <br />
         <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
       </h1>
